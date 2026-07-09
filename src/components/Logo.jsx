7737c64efaf2375d21e2485ace946f18e41add3c
gabriel24-lab <ricardomejia.joseumar@gmail.com>
2026-07-09 11:21:22 -0500

    Logo remodelado

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,7 @@
 export default function Logo() {
   return (
     <a className="brand" href="#inicio" aria-label="Ir al inicio">
+      <img src="/icon%20principal.jpeg" alt="Icono Gabriel Mejia" className="brand-img" />
       <span className="brand-text">Gabriel Mejia</span>
     </a>
   );
